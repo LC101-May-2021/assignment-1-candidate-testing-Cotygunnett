216a00d635130058f172cbf0112508000602b225
CotyGunnett <>
2021-05-28 18:30:47 +0000

    Added rest of quiz. Ready to add grading

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,17 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
+let question = ["Who was the first American woman in space? ",
+                "True or false: 5 kilometer == 5000 meters?",
+                 "(5 + 3)/2 * 10 = ?",
+              "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+                "What is the minimum crew size for the ISS?"];
+let correctAnswer = ["Sally Ride",
+                      "true",
+                      "40",
+                      "Trajectory",
+                      "3"];
+let candidateAnswer = [""];
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -20,10 +28,13 @@ candidateName = input.question("Please enter your name.")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
- candidateAnswer = input.question(question);
+// candidateAnswer = input.question(question);
 
+
+for (let i = 0; i < question.length; i++){
+  candidateAnswer.push(input.question(question[i]));
 }
-
+}
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
