45595070ff501eeb197639d20748310a457533cc
CotyGunnett <>
2021-06-02 03:03:50 +0000

    nothing

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,16 +5,16 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
+let question = "Who was the first American woman in space? " ;
 let correctAnswer;
-let candidateAnswer = ""
+let candidateAnswer = " ";
 let questions = ["Who was the first American woman in space? ",
                 "True or false: 5 kilometer == 5000 meters? ",
                  "(5 + 3)/2 * 10 = ? ",
               "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
                 "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride",
-                      "True",
+                      "true",
                       "40",
                       "Trajectory",
                       "3"];
